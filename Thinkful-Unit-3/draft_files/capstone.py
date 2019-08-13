7b52bbfb1b31ca57d69fa1c1f91cb41a227eed44
Kathy Trieu <38932563+itstrieu@users.noreply.github.com>
2019-08-13 14:59:27 -0700

    -

--- Thinkful-Unit-3/draft_files/capstone.py
+++ Thinkful-Unit-3/draft_files/capstone.py
@@ -9,8 +9,25 @@ from sklearn.model_selection import cross_val_score
 
 # Begin data exploration
 os.chdir("/Users/Mango/Documents/GitHub/Thinkful/Thinkful-Unit-3/nyt")
+
+# Each dataset contains either articles or article comments per year. They need to be concatenated.
+# First, I'll combine all articles into one dataset, and comments into another, with a column for the year the article was published.
+
+
 df = pd.read_csv('ArticlesApril2017.csv')
 df2 = pd.read_csv('CommentsApril2017.csv')
 
+import re
 
-df2.articleID.unique
+REPLACE_NO_SPACE = re.compile("[.;:!\'?,\"()\[\]]")
+REPLACE_WITH_SPACE = re.compile("(<br\s*/><br\s*/>)|(\-)|(\/)")
+
+def preprocess_comments(commentBody):
+    commentBody = [REPLACE_NO_SPACE.sub("", line.lower()) for line in commentBody]
+    commentBody = [REPLACE_WITH_SPACE.sub(" ", line) for line in commentBody]
+
+    return commentBody
+
+df2.commentBody = preprocess_comments(df2.commentBody)
+
+commentBody = df2.commentBody
